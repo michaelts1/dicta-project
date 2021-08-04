0d0d9be4993827ef3206caa9b8404c3272e529cc
michaelts1 <41857330+michaelts1@users.noreply.github.com>
2021-08-04 17:03:20 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,124 @@
+import { pad, getMasechet/*, levenshtein*/ } from "./modules.js";
+
+const mishnaKey = "מתני׳";
+
+function query(masechet, searchQuery) {
+	/* Find the mishna that contains the query */
+
+	/* This regex was inspired by https://stackoverflow.com/a/8374980,
+		and captures the last mishna that contains the search query		*/
+	const firstMishnaRegExp = new RegExp(mishnaKey + "(?:[^:](?!" + mishnaKey + "))+?" + searchQuery + "[^:]*?:");
+	const firstMishna = masechet.match(firstMishnaRegExp);
+
+	if (firstMishna === null) {
+		$("#results-count-total, #results-count-consecutive").text(0);
+		$("#results-container").show();
+		$("#results").append(`<li style="display: block;">הביטוי שהזנת לא נמצא במשנה</li>`);
+		return;
+	}
+
+	const endOfFirstMishna = firstMishna.index + firstMishna[0].length;
+	let startOfNextMishna = masechet.indexOf(" " + mishnaKey, endOfFirstMishna);
+	if (startOfNextMishna === -1) startOfNextMishna = masechet.length;
+
+	masechet = masechet.substring(firstMishna.index, startOfNextMishna);
+
+	/* Split the masechet at every colon and store all segments */
+	const colonRegExp = /:/g;
+	const colonsIndexes = [];
+
+	let execResult = null;
+	while ((execResult = colonRegExp.exec(masechet)) !== null) {
+		colonsIndexes.push(execResult.index);
+	}
+
+	const segments = [];
+	const mishna = masechet.substring(0, colonsIndexes[0]);
+
+	// Store segments (ignore the last colon index)
+	for (let i = 0; i < colonsIndexes.length-1; i++) {
+		const indexes = [colonsIndexes[i], colonsIndexes[i + 1]];
+		segments.push({
+			indexes,
+			result: masechet.substring(...indexes),
+		});
+	}
+
+	/* Compare each segment to the original mishna */
+	const citations = [];
+	for (const segment of segments) {
+		const strippedText = segment.result
+			.replace(/^: ו?/, "")
+			.replace(/ וכו'\s*:?$/, "");
+
+		if (mishna.indexOf(strippedText) !== -1) citations.push(segment);
+	}
+
+	/* Find consecutive citations that contain the query */
+	for (const citation of citations) {
+		citation.isWanted = citation.result.indexOf(searchQuery) !== -1;
+	}
+
+	let maxConsecutive = 0;
+	for (let i = 0, consecutive = 0; i < citations.length; i++) {
+		if (citations[i].isWanted) {
+			consecutive++;
+		} else {
+			consecutive = 0;
+		}
+
+		if (consecutive > maxConsecutive) {
+			maxConsecutive = consecutive;
+		}
+	}
+
+	/* Output results */
+	for (const citation of citations) {
+		// Wrap the citation with some of the text before and after it:
+		let text = masechet
+			.substring(citation.indexes[0] - 50, citation.indexes[1] + 50);
+
+		if (citation.isWanted) {
+			text = text.split(searchQuery);
+			text = pad(text[0], `<strong>${searchQuery}</strong>`, text[1], 40);
+		} else {
+			text = text.split(":");
+			text = pad(text[0] + ":", text[1] + ":", text[2], 40);
+		}
+
+		$("#results").append(`<li class=${citation.isWanted ? "wanted" : "unwanted"}>${text}</li>`);
+	}
+
+	if (citations.filter(citation => citation.isWanted).length === 0) {
+		$("#results").append(`<li style="display: block;">
+			הביטוי שהזנת לא צוטט בגמרא לאחר המשנה שמתחילה במילים
+			"${pad("", "", mishna, 50)}"
+		</li>`);
+	}
+
+	$("#results-count-total").text($(".wanted").length);
+	$("#results-count-consecutive").text(maxConsecutive);
+
+	$("#results-container").show();
+}
+
+$("#search-button").click(() => {
+	$("#results").empty();
+	$("#results-container").hide();
+
+	const searchQuery = $("#search-text").val();
+	if (searchQuery.length < 4) {
+		window.alert("הביטוי חייב להכיל לפחות 4 תווים");
+		return;
+	}
+
+	getMasechet("Nezikin", "Bava Batra").then(value => {
+		query(value, searchQuery);
+	}, reason => {
+		alert("החיפוש נכשל.\n", reason);
+	});
+});
+
+$("#show-unwanted").click(()=>{
+	$(".unwanted").css("display", "list-item");
+});
---
+++ src/modules.js
@@ -0,0 +1,242 @@
+/* A bundle of different modules for this project */
+
+/*		-- get-masechet --
+	Returns a string containing a whole Talmud masechet
+	MIT License. Copyright (c) 2021 Michael Tsaban */
+export const getMasechet = (function() {
+	const talmudTree = {
+		Zeraim: {
+			Berakhot: [],
+			//Peah: [], -- does not exist on Sefaria
+			//Demai: [], -- does not exist on Sefaria
+			//Kilayim: [], -- does not exist on Sefaria
+			//Sheviit: [], -- does not exist on Sefaria
+			//Terumot: [], -- does not exist on Sefaria
+			//Maaserot: [], -- does not exist on Sefaria
+			//Challa: [], -- does not exist on Sefaria
+			//Orlah: [], -- does not exist on Sefaria
+			//Bikkurim: [], -- does not exist on Sefaria
+		},
+		Moed: {
+			Shabbat: [],
+			Eruvin: [],
+			Pesachim: [],
+			//Shekalim: [], -- does not exist on Sefaria
+			Yoma: [],
+			Sukkah: [],
+			Beitzah: [],
+			"Rosh Hashanah": [],
+			Taanit: [],
+			Megillah: [],
+			"Moed Katan": [],
+			Chagigah: [],
+		},
+		Nashim: {
+			Yevamot: [],
+			Ketubot: [],
+			Nedarim: [],
+			Nazir: [],
+			Sotah: [],
+			Gittin: [],
+			Kiddushin: [],
+		},
+		Nezikin: {
+			"Bava Kamma": [],
+			"Bava Metzia": [],
+			"Bava Batra": [],
+			Sanhedrin: [],
+			Makkot: [],
+			Shevuot: [],
+			//Eduyot: [], -- does not exist on Sefaria
+			"Avodah Zarah": [],
+			//Pirkei Avot: [], -- does not exist on Sefaria
+			Horayot: [],
+		},
+		Kodashim: {
+			Zevachim: [],
+			Menachot: [],
+			Chullin: [],
+			Bekhorot: [],
+			Arakhin: [],
+			Temurah: [],
+			Keritot: [],
+			Meilah: [],
+			Tamid: [],
+			//Middot: [], -- does not exist on Sefaria
+			//Kinnim: [], -- does not exist on Sefaria
+		},
+		Tahorot: {
+			//Keilim: [], -- does not exist on Sefaria
+			//Oholot: [], -- does not exist on Sefaria
+			//Negaim: [], -- does not exist on Sefaria
+			//Parah: [], -- does not exist on Sefaria
+			//Tohorot: [], -- does not exist on Sefaria
+			//Mikvaot: [], -- does not exist on Sefaria
+			Niddah: [],
+			//Makshirin: [], -- does not exist on Sefaria
+			//Zavim: [], -- does not exist on Sefaria
+			//Tevul Yom: [], -- does not exist on Sefaria
+			//Yadayim: [], -- does not exist on Sefaria
+			//Uktim: [], -- does not exist on Sefaria
+		},
+	};
+
+	return async function(seder, masechet) {
+		if (!talmudTree?.[seder]?.[masechet]) throw TypeError("Invalid seder or masechet provided (not all masechtot are supported right now)");
+
+		// Get from cache:
+		if (talmudTree[seder][masechet].length !== 0) return talmudTree[seder][masechet];
+
+		// Get from Sefaria (JSON format):
+		const url = `https://raw.githubusercontent.com/Sefaria/Sefaria-Export/master/json/Talmud/Bavli/Seder%20${seder}/${masechet}/Hebrew/merged.json`;
+		await $.get(url, data => {
+			masechet = data;
+		}, "json");
+
+		// Join all parts of the amudim:
+		for (let i = 0; i < masechet.text.length; i++) masechet.text[i] = masechet.text[i].join(" ");
+
+		// Join all amudim:
+		masechet.text = masechet.text.join(" ");
+
+		// Strip off unrelated text:
+		masechet.text = masechet.text
+			.replace(/[<>\\a-z/]/g, "") // html tags e.g. <big>, </strong> etc.
+			.replace(/\s{2,}/g, " "); // white space
+
+		// Cache for later use:
+		talmudTree[seder][masechet] = masechet.text;
+
+		return talmudTree[seder][masechet];
+	};
+})();
+
+/*		-- pad-text --
+	Shorten a string without truncating words - Based on https://stackoverflow.com/a/40382963
+	MIT License. Copyright (c) 2021 Michael Tsaban */
+export const pad = (function() {
+	function _shortenEnd(str, len) {
+		return str.length <= len ?
+			str :
+			str.substr(0, str.lastIndexOf(" ", len));
+	}
+
+	function _shortenStart(str, len) {
+		return str.length <= len ?
+			str :
+			_shortenStart( // recursively remove the first word
+				str.substring(
+					str.indexOf(" ")+1), len);
+	}
+
+	return function(startString, mainString, endString, padSize) {
+		return _shortenStart(startString, padSize) + mainString + _shortenEnd(endString, padSize);
+	};
+})();
+
+/*		-- js-levenshtein --
+	Taken and modified from: https://github.com/gustf/js-levenshtein/blob/master/index.js
+	MIT License. Copyright (c) 2017 Gustaf Andersson */
+export const levenshtein = (function()
+{
+	function _min(d0, d1, d2, bx, ay)
+	{
+		return d0 < d1 || d2 < d1
+			? d0 > d2
+				? d2 + 1
+				: d0 + 1
+			: bx === ay
+				? d1
+				: d1 + 1;
+	}
+
+	return function(a, b)
+	{
+		if (a === b) {
+			return 0;
+		}
+
+		if (a.length > b.length) {
+			var tmp = a;
+			a = b;
+			b = tmp;
+		}
+
+		var la = a.length;
+		var lb = b.length;
+
+		while (la > 0 && (a.charCodeAt(la - 1) === b.charCodeAt(lb - 1))) {
+			la--;
+			lb--;
+		}
+
+		var offset = 0;
+
+		while (offset < la && (a.charCodeAt(offset) === b.charCodeAt(offset))) {
+			offset++;
+		}
+
+		la -= offset;
+		lb -= offset;
+
+		if (la === 0 || lb < 3) {
+			return lb;
+		}
+
+		var x = 0;
+		var y;
+		var d0;
+		var d1;
+		var d2;
+		var d3;
+		var dd;
+		var dy;
+		var ay;
+		var bx0;
+		var bx1;
+		var bx2;
+		var bx3;
+
+		var vector = [];
+
+		for (y = 0; y < la; y++) {
+			vector.push(y + 1);
+			vector.push(a.charCodeAt(offset + y));
+		}
+
+		var len = vector.length - 1;
+
+		for (; x < lb - 3;) {
+			bx0 = b.charCodeAt(offset + (d0 = x));
+			bx1 = b.charCodeAt(offset + (d1 = x + 1));
+			bx2 = b.charCodeAt(offset + (d2 = x + 2));
+			bx3 = b.charCodeAt(offset + (d3 = x + 3));
+			dd = (x += 4);
+			for (y = 0; y < len; y += 2) {
+				dy = vector[y];
+				ay = vector[y + 1];
+				d0 = _min(dy, d0, d1, bx0, ay);
+				d1 = _min(d0, d1, d2, bx1, ay);
+				d2 = _min(d1, d2, d3, bx2, ay);
+				dd = _min(d2, d3, dd, bx3, ay);
+				vector[y] = dd;
+				d3 = d2;
+				d2 = d1;
+				d1 = d0;
+				d0 = dy;
+			}
+		}
+
+		for (; x < lb;) {
+			bx0 = b.charCodeAt(offset + (d0 = x));
+			dd = ++x;
+			for (y = 0; y < len; y += 2) {
+				dy = vector[y];
+				vector[y] = dd = _min(dy, d0, dd, bx0, vector[y + 1]);
+				d0 = dy;
+			}
+		}
+
+		return dd;
+	};
+})();
